48601e11da7e09f8fd403084dd5bd7431d952398
mrioshe <mrioshe@unal.edu.co>
2025-10-13 12:05:06 -0500

    ex 8_3 started

---
+++ Ejercicio8_3/src/main/java/com/mycompany/ejercicio8_3/Ejercicio8_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio8_3;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Ejercicio8_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Ejercicio8_3/src/main/java/com/mycompany/ejercicio8_3/FiguraGeometrica.java
@@ -0,0 +1,34 @@
+package com.mycompany.ejercicio8_3;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Mauricio
+ */
+public class FiguraGeometrica {
+    
+    private double volumen;
+    private double superficie;
+    
+    public void setVolumen(double volumen){
+        this.volumen=volumen;
+    }
+    
+    public void setSuperficie(double superficie){
+        this.superficie=superficie;
+    }
+    
+    public double getVolumen(){
+        return this.volumen;
+    }
+    
+    public double getSuperficie(){
+        return this.superficie;
+    }
+    
+    
+}
