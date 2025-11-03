9dfd57020ce60e4572e1f278ae8e311666a3478b
mrioshe <mrioshe@unal.edu.co>
2025-11-03 16:30:41 -0500

    EjercicioP427 Done

---
+++ EjercicioP427/src/main/java/com/mycompany/ejerciciop427/EjercicioP427.java
@@ -0,0 +1,11 @@
+package com.mycompany.ejerciciop427;
+import java.io.*;
+
+
+public class EjercicioP427 {
+
+        public static void main(String[] args) throws Exception {
+        VentanaPrincipal ventana=new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
