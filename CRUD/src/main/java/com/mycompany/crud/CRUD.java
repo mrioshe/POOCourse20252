3b9cd338ad3d3e3314818d8e6616d1f60d9f286d
mrioshe <mrioshe@unal.edu.co>
2025-11-24 22:44:24 -0500

    created done

---
+++ CRUD/src/main/java/com/mycompany/crud/CRUD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class CRUD {
+
+        public static void main(String[] args) throws Exception {
+        VentanaPrincipal ventana=new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+        
+}
